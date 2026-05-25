5292ab83ab57ca63c474003a54a964dc0d8b5257
Enrique Barbosa <enrique.barbosasilva@gmail.com>
2026-05-25 15:41:36 -0300

    fix(importMap): add CollectionCards to importMap for Payload integration

--- src/app/(payload)/admin/importMap.ts
+++ src/app/(payload)/admin/importMap.ts
@@ -1,3 +1,6 @@
 import type { ImportMap } from 'payload'
+import { CollectionCards as CollectionCards_0 } from '@payloadcms/next/rsc'
 
-export const importMap: ImportMap = {}
+export const importMap: ImportMap = {
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_0
+}
