1ba74a23c3da3be5e8fdefb12ea9743c29b35ffb
Enrique Barbosa <enrique.barbosasilva@gmail.com>
2026-05-24 21:30:22 -0300

    fix(actions): pass importMap to handleServerFunctions

--- src/app/(payload)/actions.ts
+++ src/app/(payload)/actions.ts
@@ -2,9 +2,10 @@
 
 import { handleServerFunctions } from '@payloadcms/next/layouts'
 import config from '@payload-config'
+import { importMap } from './admin/importMap'
 import type { ServerFunctionArgs } from 'payload'
 
 export const serverFunction = async function (args: ServerFunctionArgs) {
   'use server'
-  return handleServerFunctions({ ...args, config })
+  return handleServerFunctions({ ...args, config, importMap })
 }
